164e7c47ab8881d985bb55492ffcc88a4b4b1ebe
Aditya <adijr9487@gmail,com>
2020-04-15 21:39:14 +0530

    Encryption commit

--- app.js
+++ app.js
@@ -3,21 +3,29 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const express = require("express");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
-app.set("view engine", "ejs");
+app.set("view engine", 'ejs');
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost:27017/Users", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email:String,
     password: String
-}
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {
+    secret: secret, 
+    encryptedFields: ['password'],
+});
 
 const User = new mongoose.model("User", userSchema);
+
 
 app.get("/", (req, res)=>{
     res.render("home");
@@ -38,17 +46,18 @@ app.post("/register", (req, res)=>{
     });
     newUser.save((err)=>{
         if(!err)
-            res.render("secrets");
+        {    res.render("secrets");
+            console.log("Successfully Loged In");
+    }
         else
             console.log(err);
     });
 })
 
 app.post("/login", (req, res)=>{
-    const email = req.body.username;
-    const password = req.body.password;
-    User.findOne({email: email}, (err, foundUser)=>{
-        if(foundUser.password === password)
+    User.findOne({email: req.body.username}, (err, foundUser)=>{
+        console.log(foundUser)
+        if(foundUser.password === req.body.password)
             res.render("secrets");
         else{
             res.send(err);
@@ -60,5 +69,5 @@ app.post("/login", (req, res)=>{
 
 
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Server Successfully started on PORT 3000");
+    console.log("Server Successfully running on PORT 3000");
 });
